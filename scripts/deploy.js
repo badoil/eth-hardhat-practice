58d25ff7b27bc0fb4797cbaf0f8b19bcc67f8fd4
oneal.oh <oneal.oh@kakaomobility.com>
2022-04-30 15:31:22 +0900

    fix: deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -27,3 +27,7 @@ const runMain = async() => {
 }
 
 runMain();
+
+
+// hardhat.config.js 맞춰주고 npx hardhat run script/deploy.js --network ropsten 해주면 배포됨
+// 그때 나오는 transactions.address 가 프론트에서 사용할 주소
